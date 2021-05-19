cd4a8c3cb9f3535dfdd6e71e31224c38178977d1
uiwpe <uiwpe.in@gmail.com>
2021-05-19 18:41:25 +0300

    Done

--- fib-prime.js
+++ fib-prime.js
@@ -8,9 +8,6 @@ const isPrime = num => {
 const fibonacci = num => {
   let phi = (1 + Math.sqrt(5)) / 2
   return Math.round(Math.pow(phi, num) / Math.sqrt(5))
-
-  // if (num <= 1) return 1
-  // return fibonacci(num - 1) + fibonacci(num - 2)
 }
 
 const nextPrimeFibonacci = input => {
